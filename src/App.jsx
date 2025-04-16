2307cb126edbc2449f46cc7d52cbdba80364c134
UdayTomar18 <udaytomar972@gmail.com>
2025-04-16 22:42:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header'
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Hero from './components/Hero';
 import Benifits from './components/Benifits';
-import Collaburation from './components/collaburation';
+import Collaburation from './components/Collaburation';
 import Services from './components/Services';
 import Pricing from './components/Pricing';
 import Roadmap from './components/Roadmap';
